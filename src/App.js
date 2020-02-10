d833875ad16ec086d2a6c9f8d4e71643334b3fbb
mrpineapples <m.miranda1219@gmail.com>
2020-02-10 14:48:36 -0500

    Adds datepicker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
 
 function App() {
+    const [startDate, setStartDate] = useState(new Date());
     return (
         <div className="App">
-            <input type="month" />
+            <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                showTimeSelect
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="MMMM d, yyyy h:mm aa"
+            />
         </div>
     );
 }
